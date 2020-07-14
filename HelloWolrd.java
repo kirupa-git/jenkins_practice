dc9a4aa83f549e500cf9e4b8414680e8e4728694
kirupa-git <63527317+kirupa-git@users.noreply.github.com>
2020-07-14 20:18:10 +0530

    Update HelloWolrd.java

--- HelloWolrd.java
+++ HelloWolrd.java
@@ -1,5 +1,5 @@
 public class HelloWolrd{  
     public static void main(String args[]){  
-     System.out.println("Hello Java from Github vcs");  
+     System.out.println("Hello Java  from Github vcs");  
     }  
 }  
